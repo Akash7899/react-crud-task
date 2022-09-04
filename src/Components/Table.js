7a3f776261c0e61252d960a311f079ca7780b69f
Akash Rathour <47224702+Akash7899@users.noreply.github.com>
2022-09-04 18:32:23 +0530

    Add files via upload
    reract-crud

---
+++ src/Components/Table.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Employee from "../Crud/Employee";
+
+const Table = () => {
+  return (
+    <div className="container mt-5">
+      <Employee />
+    </div>
+  );
+};
+export default Table;
