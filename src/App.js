b7d66f237f8703f990815d140e51ff5b075ccef5
Akash Rathour <47224702+Akash7899@users.noreply.github.com>
2022-09-04 18:34:55 +0530

    Add files via upload
    react crud

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import Table from "./Components/Table";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+export default App;
